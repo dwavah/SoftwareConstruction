4326fbc730ae577b943500d5e958316ac48b1028
Daniel Wavamunno <danl.wavah@gmail.com>
2025-02-21 12:03:51 +0300

    updating urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+'''from django.contrib import admin
 from django.urls import path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+'''
+
+from django.urls import path
+from users.views import UserListCreate
+from tasks.views import TaskListCreate
+
+urlpatterns = [
+    path('users/', UserListCreate.as_view(), name='users-list'),
+    path('tasks/', TaskListCreate.as_view(), name='tasks-list'),
 ]
