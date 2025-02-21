f3292636cd718ce8c09be111fed771bc85a818bb
Daniel Wavamunno <danl.wavah@gmail.com>
2025-02-21 14:14:48 +0300

    correcting spelling error from AP to API🤞

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .models import CustomUser
 from .serializers import UserSerializer
 
-class UserListCreate(generics.ListCreateAPView):
+class UserListCreate(generics.ListCreateAPIView):
     querryset = CustomUser.objects.all()
     serializer_class = UserSerializer
 
