eb235e0419343fbbd6bdf09ad7fb9984819b20eb
Daniel Wavamunno <danl.wavah@gmail.com>
2025-03-03 11:11:13 +0300

    updating

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -2,6 +2,6 @@ from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
 
-class TaskListCreate(generics.ListCreateAPIView):
+class TaskListCreateView(generics.ListCreateAPIView):
     querryset = Task.objects.all()
     serializer_class = TaskSerializer
